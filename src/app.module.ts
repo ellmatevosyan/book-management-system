259085b292a07c89d68d0084f7861e1a5ac3a2a6
Elen Matevosyan <elen.matevosyan90@gmail.com>
2025-05-15 13:23:42 +0400

    Adding Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './user/users.module';
+import { AuthorModule } from './author/author.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { UsersModule } from './user/users.module';
       synchronize: true,
     }),
     UsersModule,
+    AuthorModule,
   ],
   controllers: [AppController],
   providers: [AppService],
